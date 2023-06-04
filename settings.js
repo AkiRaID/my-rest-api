c0227529e771b3aef30f5e973f43ae243ed031e4
AkiRa <79385860+AkiRaID@users.noreply.github.com>
2023-06-04 08:25:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
+global.creator = 'Akira ID'// yourname
 global.MONGO_DB_URI = "" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "muhammadreihansaputra1111@gmail.com" //email
+global.email_password = "pertamina" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
